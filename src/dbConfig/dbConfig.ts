311ef2171da12281e0249c648f210a502e93ec46
Muhammad Huzaifa <dev.muhammadhuzaifa@gmail.com>
2024-07-18 18:45:49 +0500

    chore: Update .gitignore and add signup route and dbConfig files

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -1,9 +1,8 @@
-import { log } from "console";
 import mongoose from "mongoose";
 
 export async function connect() {
     try {
-        mongoose.connect(process.env.MONGODB_URI! )
+        mongoose.connect(process.env.MONGO_URI! )
         const connection = mongoose.connection;
         connection.once('open', () => {
             console.log('Database connected');
